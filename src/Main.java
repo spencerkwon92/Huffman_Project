ace0316454e333510cab48cc94945e0e2fd05ad7
spencerkwon92 <56502807+spencerkwon92@users.noreply.github.com>
2021-03-30 08:28:56 +0900

    Trial3 : Almost DONE!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
-import java.nio.file.Files;
+import java.nio.charset.*;
+import java.nio.file.*;
 import java.util.*;
 import java.util.stream.Stream;
 /**
@@ -41,33 +42,70 @@ public class Main {
     private static Scanner scan = new Scanner(System.in);
     private static Map<Character, String> prefixCodeTable = new HashMap<>();
     public static int bitwight = 0;
+    public static FileOutputStream fos;
 
-    Main() throws IOException {
-        String path = "/Users/sungjin.spencerkwon/Desktop/univ/2021 Winter Semester/" +
-                "CS 301/Final project referrence/data/plaintext/palindrome.txt";
-        File file = new File(path);
-
-        BufferedReader br =new BufferedReader(new FileReader(file));
-        String line;
-        String encodeData = null;
-        while((line=br.readLine())!=null) {
-            encodeData = encoding(line);
+    static {
+        try {
+            fos = new FileOutputStream("output.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        System.out.println("****");
-        System.out.println(bitwight);
     }
 
-//    Main(){
-//        while(scan.hasNext()){
-//            String data = scan.nextLine();
-//            System.out.println(data);
-//            System.out.println("Original Input Data : " + data);
-//        }
+//    Main() throws IOException {
+////        while(scan.hasNext()){
+////            String data = scan.nextLine();
+////            System.out.println(data);
+////            System.out.println("Original Input Data : " + data);
+////        }
+//
+//        String data = "Was it a rat I saw?\n";
 //
 //        String encodeData = encoding(data);
-//        System.out.println("Encoded Data : " + encodeData);
+//        String division = "****"+"\n";
+//        fos.write(division.getBytes());
+//        String bits = String.valueOf(bitwight)+"\n";
+//        fos.write(bits.getBytes());
+//
+//        for(int i=0; i<encodeData.length(); i+=8){
+//            String singleByte = encodeData.substring(i,i+8);
+//            int value = Integer.parseInt(singleByte,2);
+//            fos.write(value);
+//        }
+//        fos.write(encodeData.getBytes());
+//
+//        fos.close();
+//
+//        System.out.println("");
+//        System.out.println(bitwight);
 //
 //    }
+
+    Main() throws IOException {
+        Path path = Paths.get("/Users/sungjin.spencerkwon/IdeaProjects/Huffman Project/src/palindrome.txt");
+
+        String data = Files.readString(path, StandardCharsets.ISO_8859_1);
+        String code = encoding(data);
+        String division = "****"+"\n";
+        fos.write(division.getBytes());
+        String bits = String.valueOf(bitwight)+"\n";
+        fos.write(bits.getBytes());
+
+//        for(int i=0; i<code.length(); i+=8){
+//            String singleByte = code.substring(i,i+8);
+//            int value = Integer.parseInt(singleByte,2);
+//            fos.write(value);
+//        }
+        fos.write(code.getBytes());
+
+        fos.close();
+
+        System.out.println("");
+        System.out.println(bitwight);
+
+
+    }
+
 
     public static Node buildTree(PriorityQueue<Node> priQue) {
         if(priQue.size() == 1) {
@@ -89,7 +127,7 @@ public class Main {
         }
     }
 
-    public static String encoding(String data) {
+    public static String encoding(String data) throws IOException {
         Map<Character, Integer> charFreq = new HashMap<>();
         for(char c:data.toCharArray()) {
             if(!charFreq.containsKey(c)) {
@@ -117,13 +155,17 @@ public class Main {
         return sb.toString();
     }
 
-    public static void setPrefixCode(Node n, String code) {
+    public static void setPrefixCode(Node n, String code) throws IOException {
         if (n==null) return;
 
         if(n.data != '-' && n.left== null && n.right == null) {
             prefixCodeTable.put(n.data, code);
-//            System.out.println("- "+n.data + "("+n.freq + ") = " + code);
-            System.out.println(code + " "+ n.data + " "+ "Frequency : "+n.freq + " "+ code.length());
+//            System.out.println("- "+n.data + "("+n.freq + ") = " + code +" "+(code.length()* n.freq));
+//            System.out.println(code + " "+ n.data);
+//            String str = "- "+n.data + "("+n.freq + ") = " + code +" "+(code.length()* n.freq)+"\n";
+            String str = code+" "+n.data+"\n";
+
+            fos.write(str.getBytes());
 
             int bit = code.length()*n.freq;
             bitwight = bitwight + bit;
