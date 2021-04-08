456d07bf3b4628a5dff4970cb62367e86d9b7147
spencerkwon92 <56502807+spencerkwon92@users.noreply.github.com>
2021-04-08 21:39:00 +0900

    Trial4: Done for the encodeing

--- src/DecodeFile.java
+++ src/DecodeFile.java
@@ -1,46 +1,166 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * Final Project-decompressing the zip301 file.
+ * By.Sungjin Kwon, Sungjoon Ha
+ *
+ * Methods Explaination....
+ * buildTree(method): building Huffman Tree.
+ * deencoding(method): decoding the text file. It recieve the binarycode and read by character by character. and when it find
+ * the value in the HASHMAP, it change it to char(key) value.
+ * getKeysByValue(method): getting key value in the HASHMAP using STREAM.
+ */
 
 public class DecodeFile {
-    private static Map<String, String> prefixCodeTale = new HashMap<>();
+    private static Map<Character, String> prefixCodeTale = new HashMap<>();
 
+    public static long TOTALTIME  = 0;
+    public static long START;
+    public static long tmp;
 
-    DecodeFile() {
+    DecodeFile(String path) throws IOException{
 
-        Scanner scan = new Scanner(System.in);
+        START = System.currentTimeMillis();
 
-        while(scan.next().equals("****")){
-            var cods = scan.next();
-            var characters = scan.next();
+        String OutPutFileName = path.substring(0,path.length()-7);
+        FileInputStream FIS = new FileInputStream(new File(path));
+        FileOutputStream FOS = new FileOutputStream(OutPutFileName+"2.txt");
 
-            System.out.println(cods + " "+ characters);
+        List<String> dataArr = new ArrayList<>();
+        String newLine;
+        String str;
+        String input = "";
+        int reading =0;
+
+        while(true){
+            String line="";
+            while((reading = FIS.read()) != -1){
+                input += (char)reading;
+                if((char)reading == '\n'){
+                    line.trim();
+                    break;
+                }
+                line += (char)reading;
+            }
+            if(line.contains("*****")){
+                break;
+            }
+
+            if(line.contains("space")&&line.contains(" ")){
+                str = line.replaceAll(" ", "|");
+                newLine = str.replaceAll("space", " ");
+                dataArr.add(newLine);
+            }else if(line.contains("newline")&&line.contains(" ")){
+                str = line.replaceAll(" ", "|");
+                newLine = str.replaceAll("newline", "\n");
+                dataArr.add(newLine);
+            }else if(line.contains("return")&&line.contains(" ")){
+                str = line.replaceAll(" ", "|");
+                newLine = str.replaceAll("return", "\r");
+                dataArr.add(newLine);
+            }else if(line.contains("tab")&&line.contains(" ")){
+                str = line.replaceAll(" ", "|");
+                newLine = str.replaceAll("tab", "\t");
+                dataArr.add(newLine);
+            }else{
+                str = line.replaceAll(" ", "|");
+                dataArr.add(str);
+            }
+        }
+        int num = dataArr.size();
+        for(int i=0; i<num; i++){
+            String[] codeSet = dataArr.get(i).split("\\|");
+            String value = codeSet[0];
+            char key = codeSet[1].charAt(0);
+            prefixCodeTale.put(key, value);
         }
 
-//        File path = new File("palindrome.zip301");
-//        FileInputStream file = new FileInputStream(path);
-//
-//        String input = "";
-//        int i;
-//        while((int i = file.read()) != -1){
-//
+        int n;
+        String weightLine = "";
+        String bitWeight = "";
+        while((n=FIS.read()) !=-1){
+            bitWeight += (char)n;
+            if((char) n== '\n'){
+                weightLine.trim();
+                break;
+            }
+            weightLine += (char)n;
+        }
+
+
+        int bitWight = Integer.valueOf(weightLine);
+        String buffer="";
+        byte[] data = FIS.readAllBytes();
+        for(var ele:data){
+            String bin = Integer.toBinaryString(Byte.toUnsignedInt(ele));
+            int len = bin.length();
+            for(int i=len; i<8;i++){
+                bin ="0"+bin;
+            }
+            buffer += bin;
+        }
+
+//        buffer = buffer.substring(0, bitWight);
+
+//        int numBytes = bitWight / 8;
+//        String buffer = "";
+//        for(int i=0; i<numBytes+2; i++){
+//            int b= FIS.read();
+//            String bin = Integer.toBinaryString(b);
+//            int numZeros = 8-bin.length();
+//            String padding = "0".repeat(numZeros);
+//            bin = padding + bin;
+//            buffer += bin;
 //        }
+        String decodeSentences = decoding(buffer);
+        FOS.write(decodeSentences.getBytes());
+        FOS.close();
+
+        tmp = System.currentTimeMillis() - START;
+        TOTALTIME  += tmp;
+
+        System.out.println("*** UNZIP301 - OUR TEAM IMPLEMENTATION ***");
+        System.out.println("Total Time :" +TOTALTIME);
+        System.out.println("Output File Name: "+OutPutFileName.substring(7, OutPutFileName.length())+"2.txt");
+        System.out.println("*** All processes are DONE ***");
 
     }
 
+    public static String decoding(String data){
 
+        START = System.currentTimeMillis();
 
-//    public static String decode(String encodeData){
-//        StringBuilder sb = new StringBuilder();
-//        String tmp = "";
-//        for(char c:encodeData.toCharArray()){
-//            tmp += c;
-//        }
-//    }
+        StringBuilder sb = new StringBuilder();
+        String buffer = "";
+        for(char c : data.toCharArray()){
+            buffer += c;
 
-    public static void main(String[] argv) {
-        new DecodeFile();
+            if(prefixCodeTale.containsValue(buffer)){
+                Stream<Character> keyStream = getKeysByValue(prefixCodeTale, buffer);
+                char key = keyStream.findFirst().get();
+                sb.append(key);
+                buffer="";
+            }
+        }
 
+        tmp = System.currentTimeMillis() - START;
+        TOTALTIME  += tmp;
+
+        return sb.toString();
+    }
+
+    public static <K, V> Stream<K> getKeysByValue(Map<K, V> map, V value) {
+        return map.
+                entrySet().
+                stream().
+                filter(entry -> value.equals(entry.getValue())).
+                map(Map.Entry::getKey);
+    }
+
+    public static void main(String[] argv) throws IOException {
+        String path = argv[0];
+        new DecodeFile("sample/"+path);
     }
 }
--- src/EncodeFile.java
+++ src/EncodeFile.java
@@ -1,21 +1,23 @@
 import java.io.*;
-import java.nio.charset.*;
-import java.nio.file.*;
 import java.util.*;
 import java.util.stream.Stream;
-/**
- * MESSAGE:
- * 성준아! 이건 내가 임의로 공부하려고 배껴온거야 ㅋㅋ 그래서 아직 적용인 안된게 많다ㅜㅜ
- * 일단 더 공부해보고 이번주까지 호프만 트리완성할게!
- *
- */
 
 /**
- * Final Project
+ * Final Project-compressing the text file.
  * By.Sungjin Kwon, Sungjoon Ha
  *
+ * Methods Explaination....
+ * Node(class): class for the Node. It has data(key), freq(frequency), and left right Node.
+ * Since we need to sort the Node when we create the tree, we implements the compareTo interface.
+ *
+ * buildTree(method): building Huffman Tree.
+ * encoding(method): encoding the text file.
+ * setPrefixCode(method): writing the code and key value and binarycode to the new text file.
+ *
+ * getKeysByValue(method): getting key value in the HASHMAP using STREAM.
+ * readDOC(method): I created it for reading text file content effectively and quickely.
  */
-// 호프만을 구현하기 위한 노드
+
 class Node implements Comparable<Node>{
     char data;
     int freq;
@@ -33,7 +35,6 @@ class Node implements Comparable<Node>{
     }
 }
 
-
 public class EncodeFile {
 
     private static Scanner scan = new Scanner(System.in);
@@ -42,19 +43,23 @@ public class EncodeFile {
     public static FileOutputStream fos;
     public static String outPutFileName;
 
+    public static long TOTALTIME  = 0;
+    public static long START;
+    public static long tmp;
+
     EncodeFile(String filename) throws IOException {
 
         try{
+            File file = new File(filename);
+            String data = readDoc(file);
 
-            Path path = Paths.get(filename);
             outPutFileName = filename.substring(0,filename.length()-4);
-
             fos = new FileOutputStream(outPutFileName+".zip301");
 
-            String data = Files.readString(path, StandardCharsets.ISO_8859_1);
             String code = encoding(data);
-            String division = "****"+"\n";
+            String division = "*****"+"\n";
             String bits = String.valueOf(bitwight) +"\n";
+
             fos.write(division.getBytes());
             fos.write(bits.getBytes());
             for(int i=0; i<code.length(); i+=8){
@@ -62,52 +67,27 @@ public class EncodeFile {
                 int value = Integer.parseInt(singleByte,2);
                 fos.write(value);
             }
-
             fos.close();
-
-
 
         }catch (IOException e){
 
-            }catch (NumberFormatException ef){
+        }catch (NumberFormatException ef){
+
+        }catch(StringIndexOutOfBoundsException str){
 
         }
 
-        System.out.println("Huffman Zip - TEAM REFERENCE IMPLEMENTATION ***");
-        System.out.println("Generating frequency data...");
-        System.out.println("Building the Huffman tree..."+(buildTreeE - buildTreeS)/1000.0 + " sec");
-        System.out.println("Generating the Huffman code...");
-        System.out.println("Encoding the document..." + (encodingE - encodingS)/1000.0 + " sec");
-        System.out.println("Writing output file..." + (prefixE - prefixS)/1000.0 + " sec");
-        System.out.println("Wrote output to: " + outPutFileName+".zip301");
-        System.out.println("TOTAL TIME: ");
+        System.out.println("*** Huffman Zip - TEAM REFERENCE IMPLEMENTATION ***");
+        System.out.println("Wrote output to: " + outPutFileName.substring(7, outPutFileName.length())+".zip301");
+        System.out.println("TOTAL TIME: " + TOTALTIME +" seconds.");
+        System.out.println("*** ALL PROCESSES ARE DONE ***");
+
     }
-
-    static long buildTreeS = System.currentTimeMillis();
-    public static Node buildTree(PriorityQueue<Node> priQue) {
-        if(priQue.size() == 1) {
-            return priQue.poll();
-        }else {
-            Node leftNode = priQue.poll();
-            Node rightNode = priQue.poll();
-            Node sumNode = new Node();
-
-            if(leftNode !=null && rightNode!=null){
-                sumNode.data = '-';
-                sumNode.freq = leftNode.freq + rightNode.freq;
-                sumNode.left = leftNode;
-                sumNode.right = rightNode;
-            }
-            priQue.offer(sumNode);
-            return buildTree(priQue);
-
-        }
-    }
-    static long buildTreeE = System.currentTimeMillis();
-
-    static long encodingS = System.currentTimeMillis();
 
     public static String encoding(String data) throws IOException {
+
+        START = System.currentTimeMillis();
+
         Map<Character, Integer> charFreq = new HashMap<>();
         for(char c:data.toCharArray()) {
             if(!charFreq.containsKey(c)) {
@@ -131,26 +111,55 @@ public class EncodeFile {
         for(char c:data.toCharArray()) {
             sb.append(prefixCodeTable.get(c));
         }
+
+        tmp = System.currentTimeMillis() - START;
+        TOTALTIME  += tmp;
+
         return sb.toString();
     }
 
-    static long encodingE = System.currentTimeMillis();
 
-    static long prefixS = System.currentTimeMillis();
+    public static Node buildTree(PriorityQueue<Node> priQue) {
+        START = System.currentTimeMillis();
+
+        if(priQue.size() == 1) {
+            return priQue.poll();
+        }else {
+            Node leftNode = priQue.poll();
+            Node rightNode = priQue.poll();
+            Node sumNode = new Node();
+
+            if(leftNode !=null && rightNode!=null){
+                sumNode.freq = leftNode.freq + rightNode.freq;
+                sumNode.left = leftNode;
+                sumNode.right = rightNode;
+            }
+            priQue.offer(sumNode);
+
+            tmp = System.currentTimeMillis() - START;
+            TOTALTIME  += tmp;
+
+            return buildTree(priQue);
+        }
+    }
+
     public static void setPrefixCode(Node n, String code) throws IOException {
+
+        START = System.currentTimeMillis();
+
         if (n==null) return;
 
-        if(n.data != '-' && n.left== null && n.right == null) {
+        if(n.left== null && n.right == null) {
             prefixCodeTable.put(n.data, code);
 
             if(n.data == '\n'){
-                fos.write((code+" newLine"+"\n").getBytes());
+                fos.write((code+" newline"+"\n").getBytes());
             }else if(n.data == ' '){
-                fos.write((code+" Space"+"\n").getBytes());
+                fos.write((code+" space"+"\n").getBytes());
             }else if(n.data == '\r'){
-                fos.write((code+" Return"+"\n").getBytes());
+                fos.write((code+" return"+"\n").getBytes());
             }else if(n.data =='\t'){
-                fos.write((code+" Tab"+"\n").getBytes());
+                fos.write((code+" tab"+"\n").getBytes());
             }else {
                 fos.write((code+" "+n.data+"\n").getBytes());
             }
@@ -163,8 +172,9 @@ public class EncodeFile {
             setPrefixCode(n.right, code+'1');
         }
 
+        tmp = System.currentTimeMillis() - START;
+        TOTALTIME  += tmp;
     }
-    static long prefixE = System.currentTimeMillis();
 
     public static <K, V> Stream<K> getKeysByValue(Map<K, V> map, V value){
         return map.
@@ -174,14 +184,30 @@ public class EncodeFile {
                 map(Map.Entry::getKey);
     }
 
+    public String readDoc(File f){
+        String text = "";
+        int read, N = 1024*1024;
+        char[] buffer = new char[N];
+
+        try{
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            while(true){
+                read = br.read(buffer,0, N);
+                text += new String(buffer,0,read);
+
+                if(read < N){
+                    break;
+                }
+            }
+
+        }catch(Exception e ){}
+
+        return text;
+    }
 
     public static void main(String[] args) throws Exception{
         String filename = args[0];// input 파일 들고 오기 위핸서 넣은거야!!
-
-        new EncodeFile(filename);
-
-//        outPutFileName = filename.substring(0,filename.length()-4);
-
+        new EncodeFile("sample/"+filename);
     }
-
 }
